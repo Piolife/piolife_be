b8548ae964f4e53f949c4bfd11bb5037fad06704
Mr.B <erhimuebru87@gmail.com>
2025-04-08 02:45:30 +0100

    remove create from  wallet service code

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -33,117 +33,117 @@ export class WalletController {
   //   return this.walletService.createWallet(userId);
   // }
 
-//   @ApiOperation({ summary: 'Deposit funds into a user wallet' })
-//   @ApiParam({ name: 'userId', required: true, description: 'User ID' })
-//   @ApiBody({
-//     schema: {
-//       type: 'object',
-//       properties: {
-//         reference: { type: 'string', example: 'txn_123456789' },
-//       },
-//     },
-//   })
-//   @ApiResponse({
-//     status: 200,
-//     description: 'Deposit successful',
-//     schema: {
-//       type: 'object',
-//       properties: {
-//         success: { type: 'boolean', example: true },
-//         message: { type: 'string', example: 'Deposit successful' },
-//         deposit: {
-//           type: 'object',
-//           properties: {
-//             amount: { type: 'number', example: 1000 },
-//             timestamp: { type: 'string', format: 'date-time' },
-//             type: { type: 'string', example: 'deposit' },
-//             transactionRef: { type: 'string', example: 'txn_123456789' },
-//             status: { type: 'string', example: 'success' },
-//           },
-//         },
-//       },
-//     },
-//   })
-// @Post(':userId/deposit')
-// async deposit(
-//   @Param('userId') userId: string,
-//   @Body('reference') reference: string,
-// ): Promise<{ success: boolean; message: string; deposit: any }> {  
+  @ApiOperation({ summary: 'Deposit funds into a user wallet' })
+  @ApiParam({ name: 'userId', required: true, description: 'User ID' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        reference: { type: 'string', example: 'txn_123456789' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Deposit successful',
+    schema: {
+      type: 'object',
+      properties: {
+        success: { type: 'boolean', example: true },
+        message: { type: 'string', example: 'Deposit successful' },
+        deposit: {
+          type: 'object',
+          properties: {
+            amount: { type: 'number', example: 1000 },
+            timestamp: { type: 'string', format: 'date-time' },
+            type: { type: 'string', example: 'deposit' },
+            transactionRef: { type: 'string', example: 'txn_123456789' },
+            status: { type: 'string', example: 'success' },
+          },
+        },
+      },
+    },
+  })
+@Post(':userId/deposit')
+async deposit(
+  @Param('userId') userId: string,
+  @Body('reference') reference: string,
+): Promise<{ success: boolean; message: string; deposit: any }> {  
 
   // const user = await this.userModel.findById(userId);
   // if (!user) {
   //   throw new NotFoundException('User not found.');
   // }
 
-  // const { isPaymentValid, amount, status } = await this.validatePayment(reference);
-  // if (!isPaymentValid) {
-  //   throw new BadRequestException('Invalid payment. Amount mismatch, invalid reference number, or reference already used.');
-  // }
+  const { isPaymentValid, amount, status } = await this.validatePayment(reference);
+  if (!isPaymentValid) {
+    throw new BadRequestException('Invalid payment. Amount mismatch, invalid reference number, or reference already used.');
+  }
 
-  // this.usedReferences.add(reference);
+  this.usedReferences.add(reference);
 
-  // if (amount === undefined || status === undefined) {
-  //   throw new BadRequestException('Invalid payment amount or status.');
-  // }
+  if (amount === undefined || status === undefined) {
+    throw new BadRequestException('Invalid payment amount or status.');
+  }
 
-//   return this.walletService.deposit(userId, amount, reference, status);
-// }
+  return this.walletService.deposit(userId, amount, reference, status);
+}
 
 
-// private async validatePayment(reference: string): Promise<{ isPaymentValid: boolean; amount?: number; status?: string }> {
-//     try {
-//       // Check if the reference has been used before
-//       if (this.usedReferences.has(reference)) {
-//         console.error('Reference has already been used:', reference);
-//         return { isPaymentValid: false };
-//       }
+private async validatePayment(reference: string): Promise<{ isPaymentValid: boolean; amount?: number; status?: string }> {
+    try {
+      // Check if the reference has been used before
+      if (this.usedReferences.has(reference)) {
+        console.error('Reference has already been used:', reference);
+        return { isPaymentValid: false };
+      }
   
-//       const response = await axios.get(
-//         `${process.env.VERIFY_PAYMENT}/transaction/verify/${reference}`,
-//         {
-//           headers: {
-//             Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`,
-//           },
-//         }
-//       );
+      const response = await axios.get(
+        `${process.env.VERIFY_PAYMENT}/transaction/verify/${reference}`,
+        {
+          headers: {
+            Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`,
+          },
+        }
+      );
   
-//       // Extract payment status and amount
-//       const isPaymentSuccessful = response.data.data.status === 'success';
-//       const status = response.data.data.status; 
-//       const amount = response.data.data.amount / 100;
+      // Extract payment status and amount
+      const isPaymentSuccessful = response.data.data.status === 'success';
+      const status = response.data.data.status; 
+      const amount = response.data.data.amount / 100;
   
-//       if (isPaymentSuccessful) {
-//         return { isPaymentValid: true, amount, status };
-//       } else {
-//         console.error('Payment verification failed:', response.data);
-//         return { isPaymentValid: false };
-//       }
-//     } catch (error) {
-//       console.error('Error validating payment with Paystack:', error);
-//       return { isPaymentValid: false };
-//     }
-//   }
+      if (isPaymentSuccessful) {
+        return { isPaymentValid: true, amount, status };
+      } else {
+        console.error('Payment verification failed:', response.data);
+        return { isPaymentValid: false };
+      }
+    } catch (error) {
+      console.error('Error validating payment with Paystack:', error);
+      return { isPaymentValid: false };
+    }
+  }
   
 
-  // @ApiOperation({ summary: 'Get user wallet balance' })
-  // @ApiParam({ name: 'userId', required: true, description: 'User ID' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Returns the user’s wallet balance',
-  //   schema: {
-  //     type: 'object',
-  //     properties: {
-  //       balance: { type: 'number', example: 5000 },
-  //     },
-  //   },
-  // })
+  @ApiOperation({ summary: 'Get user wallet balance' })
+  @ApiParam({ name: 'userId', required: true, description: 'User ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the user’s wallet balance',
+    schema: {
+      type: 'object',
+      properties: {
+        balance: { type: 'number', example: 5000 },
+      },
+    },
+  })
 
-  // @UseGuards(JwtAuthGuard)
-  // @Get(':userId/balance')
-  // async getWalletBalance(@Param('userId') userId: string): Promise<{ balance: number }> {
-  //   const walletBalance = await this.walletService.getWalletBalance(userId);
-  //   return { balance: walletBalance };
-  // }
+  @UseGuards(JwtAuthGuard)
+  @Get(':userId/balance')
+  async getWalletBalance(@Param('userId') userId: string): Promise<{ balance: number }> {
+    const walletBalance = await this.walletService.getWalletBalance(userId);
+    return { balance: walletBalance };
+  }
 
 
 }
--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -26,98 +26,98 @@ export class WalletService {
   
 
 
-// async deposit(userId: string, amount: number, reference: string, status: string) {
-//     // const user = await this.userModel.findById(userId);
-//     // if (!user) {
-//     //   throw new NotFoundException('User not found.');
-//     // }
+async deposit(userId: string, amount: number, reference: string, status: string) {
+    // const user = await this.userModel.findById(userId);
+    // if (!user) {
+    //   throw new NotFoundException('User not found.');
+    // }
   
-//     const wallet = await this.walletModel.findOne({ userId });
-//     if (!wallet) {
-//       throw new NotFoundException('Wallet not found for user.');
-//     }
+    const wallet = await this.walletModel.findOne({ userId });
+    if (!wallet) {
+      throw new NotFoundException('Wallet not found for user.');
+    }
   
-//     // Create new deposit transaction
-//     const depositTransaction = {
-//       amount,
-//       timestamp: new Date(),
-//       type: 'deposit',
-//       transactionRef: reference,
-//       status: status,
-//     };
+    // Create new deposit transaction
+    const depositTransaction = {
+      amount,
+      timestamp: new Date(),
+      type: 'deposit',
+      transactionRef: reference,
+      status: status,
+    };
   
-//     // Update wallet with new transaction
-//     const updatedWallet = await this.walletModel.findOneAndUpdate(
-//       { userId },
-//       {
-//         $push: { transactions: depositTransaction },
-//         $inc: { balance: amount },
-//       },
-//       { new: true, useFindAndModify: false }
-//     );
+    // Update wallet with new transaction
+    const updatedWallet = await this.walletModel.findOneAndUpdate(
+      { userId },
+      {
+        $push: { transactions: depositTransaction },
+        $inc: { balance: amount },
+      },
+      { new: true, useFindAndModify: false }
+    );
   
-//     if (!updatedWallet) {
-//       throw new NotFoundException('Failed to update wallet.');
-//     }
+    if (!updatedWallet) {
+      throw new NotFoundException('Failed to update wallet.');
+    }
   
-//     return {
-//       success: true,
-//       message: 'Deposit successful',
-//       deposit: depositTransaction,
-//     };
-//   }
+    return {
+      success: true,
+      message: 'Deposit successful',
+      deposit: depositTransaction,
+    };
+  }
   
   
-  // async getWalletByUserId(userId: string): Promise<WalletDocument> {
-  //   const wallet = await this.walletModel.findOne({ userId }).exec();
-  //   if (!wallet) {
-  //     throw new NotFoundException('Wallet not found for user');
-  //   }
-  //   return wallet;
-  // }
+  async getWalletByUserId(userId: string): Promise<WalletDocument> {
+    const wallet = await this.walletModel.findOne({ userId }).exec();
+    if (!wallet) {
+      throw new NotFoundException('Wallet not found for user');
+    }
+    return wallet;
+  }
 
 
-  // async getWalletBalance(userId: string): Promise<number> {
-  //   const wallet = await this.walletModel.findOne({ userId });
+  async getWalletBalance(userId: string): Promise<number> {
+    const wallet = await this.walletModel.findOne({ userId });
 
-  //   if (!wallet) {
-  //     throw new NotFoundException('Wallet not found');
-  //   }
-  //   return wallet.balance;
-  // }
+    if (!wallet) {
+      throw new NotFoundException('Wallet not found');
+    }
+    return wallet.balance;
+  }
 
-  // async getWallet(userId: string): Promise<WalletDocument> {
-  //   let wallet = await this.walletModel.findOne({ userId });
-  //   if (!wallet) {
-  //     wallet = new this.walletModel({ userId, balance: 0, loanEligibility: 20000 });
-  //     await wallet.save();
-  //   }
-  //   return wallet;
-  // }
+  async getWallet(userId: string): Promise<WalletDocument> {
+    let wallet = await this.walletModel.findOne({ userId });
+    if (!wallet) {
+      wallet = new this.walletModel({ userId, balance: 0, loanEligibility: 20000 });
+      await wallet.save();
+    }
+    return wallet;
+  }
 
-  // async reduceLoanEligibility(userId: string, amount: number): Promise<Wallet> {
-  //   const wallet = await this.getWallet(userId);
+  async reduceLoanEligibility(userId: string, amount: number): Promise<Wallet> {
+    const wallet = await this.getWallet(userId);
 
-  //   if (wallet.loanEligibility < amount) {
-  //     throw new Error('You are not eligible for loan at this time');
-  //   }
-  //   wallet.loanEligibility -= amount;
-  //   await wallet.save();
-  //   return wallet;
-  // }
+    if (wallet.loanEligibility < amount) {
+      throw new Error('You are not eligible for loan at this time');
+    }
+    wallet.loanEligibility -= amount;
+    await wallet.save();
+    return wallet;
+  }
 
-  // async addFunds(userId: string, amount: number): Promise<Wallet> {
-  //   const wallet = await this.getWallet(userId);
-  //   wallet.balance += amount;
-  //   await wallet.save();
-  //   return wallet;
-  // }
+  async addFunds(userId: string, amount: number): Promise<Wallet> {
+    const wallet = await this.getWallet(userId);
+    wallet.balance += amount;
+    await wallet.save();
+    return wallet;
+  }
 
-  // async deductFunds(userId: string, amount: number): Promise<void> {
-  //   const wallet = await this.walletModel.findOne({ userId });
-  //   if (!wallet) throw new NotFoundException('Wallet not found.');
-  //   wallet.balance -= amount;
-  //   await wallet.save();
-  // }
+  async deductFunds(userId: string, amount: number): Promise<void> {
+    const wallet = await this.walletModel.findOne({ userId });
+    if (!wallet) throw new NotFoundException('Wallet not found.');
+    wallet.balance -= amount;
+    await wallet.save();
+  }
   
 }
